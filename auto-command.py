d850c0ce3a255f63bc9115691aea4226f714a0ca
Romain <55132443+Blaster91@users.noreply.github.com>
2020-03-27 14:32:48 +0100

    Add files via upload

---
+++ auto-command.py
@@ -0,0 +1,28 @@
+# importer les librairy
+
+
+
+# creer la fonction qui va écrire les commandes prédéfini dans un fichier texte ou en dure dans le code (on récupére et on affiche dans la console toutes les secondes les une a la suite des autres)
+
+
+command = ["#spawnitem Baseball_Cap_01","#spawnitem Pilot_Glasses","#spawnitem StabProof_Vest_04","#spawnitem Backpack_02_01","#spawnitem Military_Shirt_06","#spawnitem Open_Finger_Gloves_01","#spawnitem Waist_Bag_Small_02","#spawnitem WorkPants_02","#spawnitem Hiking_Boots_02","#spawnitem Lighter","#spawnitem MRE_Stew","#spawnitem MRE_TunaSalad","#spawnitem Water_05L 2","#spawnitem Banana","#spawnitem BP_Compass_Basic","#spawnitem BP_Weapon_590A11","#spawnitem 12_Gauge_Ammobox_Closed 3","#spawnitem 1H_Bushman","#spawnitem Emergency_Bandage 2"]
+
+def commandAll():
+    for value in variable:
+        print(command[0])
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# creer une fonction qui va prendre en paramètre les commandes et qui va les écrires à notre place. surement via un copier coller
